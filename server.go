f41225a69ca2cd3f00eddb5fc9e9a01de6569d63
Weidong Zhao <40237330+wzhao60718@users.noreply.github.com>
2019-07-04 16:10:30 -0400

    Update server.go

--- server.go
+++ server.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
+var current_dir = "."
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+  fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+  fmt.Fprintf(w, "Current directory is %s!", current_dir)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server running...")
-	http.ListenAndServe(":8080", nil)
+  pwd, err := os.Getwd()
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  //fmt.Println(pwd)
+  current_dir = pwd
+
+  http.HandleFunc("/", handler)
+  fmt.Println("Server running...")
+  http.ListenAndServe(":8080", nil)
 }
