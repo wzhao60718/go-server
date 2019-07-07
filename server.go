2ff2c179370941deb202523aecfc9dd7e28e5352
Weidong Zhao <wdz0102@gmail.com>
2019-07-07 05:19:08 +0000

    Image/JSON Dirs and Files

--- server.go
+++ server.go
@@ -31,13 +31,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	imagesDir := currentDir + "/images"
 
-	//if strings.HasSuffix(r.URL.Path[1:], "/go") {
-	fmt.Fprintf(w, "Hello %s!", currentDir)
-	fmt.Fprintf(w, "Hello Go %s!", r.URL.Path[1:])
-	//}
-	//if strings.HasSuffix(r.URL.Path[1:], "/go/images") {
-	fmt.Fprintf(w, "Hello Images %s!", imagesDir)
-	//}
+	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+	if strings.HasSuffix(r.URL.Path[1:], "/go") {
+		fmt.Fprintf(w, "Hello GO %s!", currentDir)
+	}
+	if strings.HasSuffix(r.URL.Path[1:], "/go/images") {
+		fmt.Fprintf(w, "Hello Images %s!", imagesDir)
+	}
 
 	publicImages, err := GetPublicImages(imagesDir)
 	if err != nil {
